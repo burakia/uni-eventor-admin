8e4c3d1c56e86d9fcbb93bc68fbef15b01d4f32a
byaldira <burak_yaldirak@hotmail.com>
2017-05-18 22:29:39 +0300

    Topluluk oluşturma status : 204  No-Content Veriyor
    Topluluk oluşturma status : 204  No-Content Veriyor
    OLMADI !!

--- uni-eventor-admin/src/component/CreateCommunity.jsx
+++ uni-eventor-admin/src/component/CreateCommunity.jsx
@@ -1,9 +1,8 @@
 
 import React, { Component } from 'react';
 import '../css/w3.css';
-
-
-
+import axios from 'axios';
+import * as AuthModule from '../App.Auth';
 
 class CreateCommunity extends Component {
       constructor(props) {
@@ -19,14 +18,51 @@ class CreateCommunity extends Component {
         
     }
     submitHandler(e) {
-        e.preventDefault();   
+      e.preventDefault();   
+       
+      AuthModule.login('testuser', 'Cem.123', () => {
+                alert('Login Success');
+            }, (error) => {
+                alert('Login Error');
+            });
+      
+      console.log(this.state);
+        axios.post('http://unieventorapi.azurewebsites.net/api/CommunityApi',this.state).then((response)=>{
+           console.log(response);
+            alert('Topluluk Eklendi');
+        }).catch((error)=>{console.log(error)})
+
+       
     }
     handleCommunityNameChange(event) {
         var CommunityName = event.target.value;
+         var currTime = new Date().toLocaleString();
         console.log(CommunityName);
-        this.setState({CommunityName});
+         this.setState(prevState => ({
+            CommunityId: '',
+            CommunityName: CommunityName,
+            DateCreated: currTime,
+            LastUpdated:currTime , 
+            FkCreatorId : 'testuser', 
+            FkResponsibleId : 'testuser'
+        }));
+        
     }
-
+    
+    handleCommunityResponsibleChange(event) {
+        var ResPerson = event.target.value;
+         var currTime = new Date().toLocaleString();
+        console.log(ResPerson);
+         this.setState(prevState => ({
+            CommunityId: '',
+            CommunityName: prevState.CommunityName,
+            DateCreated: currTime,
+            LastUpdated:currTime , 
+            FkCreatorId : 'testuser', 
+            FkResponsibleId : ResPerson
+        }));
+        
+    }
 
 
     render() {
@@ -39,10 +75,10 @@ class CreateCommunity extends Component {
                 <div className="w3-row w3-section">
                     <div className="w3-threequarter">
                         <div className="w3-container w3-half">
-                            <input className="w3-input w3-border w3-padding" type="text"placeholder="Topluluk Adi"  id="txtCommunityName" />
+                            <input className="w3-input w3-border w3-padding" onChange={this.handleCommunityNameChange.bind(this)} type="text"placeholder="Topluluk Adi"  id="txtCommunityName" />
                         </div>
                         <div className="w3-container w3-half">
-                            <input className="w3-input w3-border w3-padding" type="text"  placeholder="Topluluk Baskani Kullanici Adi"  id="txtUniName" />
+                            <input className="w3-input w3-border w3-padding" onChange={this.handleCommunityResponsibleChange.bind(this)} type="text"  placeholder="Topluluk Baskani Kullanici Adi"  id="txtUniName" />
                         </div>
                     </div>
                     <div className="w3-quarter"></div>
